27f960fcb50277d7aafc97372dc42caeb0a8632e
alex <nalleballeuwu@gmail.com>
2025-02-11 17:52:32 +0100

    added more fields to the user model to align it with our er-diagram

--- src/main/java/com/java2024/ecoscape/models/User.java
+++ src/main/java/com/java2024/ecoscape/models/User.java
@@ -2,7 +2,9 @@ package com.java2024.ecoscape.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 @Entity
@@ -12,8 +14,10 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
-    @NotEmpty(message = "Username cannot be empty")
+    @Column(unique = true, nullable = false, name = "email")
+    @NotEmpty(message = "Email cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
+    message = "That's not a valid email.")
     private String username;
 
     @Column(nullable = false)
@@ -22,6 +26,19 @@ public class User {
             message = "Password must be at least 8 characters long and contain at least " +
                     "one uppercase letter, one number, and one special character")*/
     private String password;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+    private String bio;
+    private String photoUrl;
+    @Column(nullable = false)
+    private LocalDate birth_date;
+    private String contact_phone_number;
+    @Pattern(regexp = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",
+            message = "That's not a valid email.")
+    private String contact_email;
 
     @ElementCollection(fetch = FetchType.EAGER)
     @Enumerated(EnumType.STRING)
@@ -62,5 +79,65 @@ public class User {
 
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public LocalDate getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(LocalDate birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public String getContact_phone_number() {
+        return contact_phone_number;
+    }
+
+    public void setContact_phone_number(String contact_phone_number) {
+        this.contact_phone_number = contact_phone_number;
+    }
+
+    public String getContact_email() {
+        return contact_email;
+    }
+
+    public void setContact_email(String contact_email) {
+        this.contact_email = contact_email;
     }
 }
