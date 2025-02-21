85abe3cc10b572c56c6941b27c5a389c9cc8ae5c
alex <nalleballeuwu@gmail.com>
2025-02-21 19:20:30 +0100

    deleted classes that belonged to another branchs issue

--- src/main/java/com/java2024/ecoscape/repository/RulesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.java2024.ecoscape.repository;
-
-import com.java2024.ecoscape.models.Rules;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RulesRepository extends JpaRepository<Rules, Long> {
-}
