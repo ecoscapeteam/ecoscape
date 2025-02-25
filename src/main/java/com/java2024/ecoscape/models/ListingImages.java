20f4ee91a4cd3259ec6ce2ff87932e4b6c830c9e
alex <nalleballeuwu@gmail.com>
2025-02-25 18:46:44 +0100

    had forgotten to add getters and setters, added the correct table name and added strategy = generation type for properauto increment in the id

--- src/main/java/com/java2024/ecoscape/models/ListingImages.java
+++ src/main/java/com/java2024/ecoscape/models/ListingImages.java
@@ -1,24 +1,46 @@
 package com.java2024.ecoscape.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 
 @Entity
-@Table(name = "listings")
+@Table(name = "listing_images")
 public class ListingImages {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name = "listing_id")
-    @NotEmpty(message = "Listing id cannot be empty.")
+    @JoinColumn(name = "listing_id", nullable = false)
     private Listing listing;
 
-    @Column(name = "image_url", unique = true)
-    @Pattern(regexp = "\\.(jpg|jpeg|png)$", message = "Only jpg, jpeg and png images are allowed to be uploaded.")
-    @NotEmpty(message = "Image url cannot be empty.")
+    @Column(name = "image_url", nullable = false)
+    @Pattern(regexp = ".*\\.(jpg|jpeg|png)$", message = "Only jpg, jpeg and png images are allowed to be uploaded.")
     private String imageUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Listing getListing() {
+        return listing;
+    }
+
+    public void setListing(Listing listing) {
+        this.listing = listing;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
+
