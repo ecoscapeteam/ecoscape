44ebf3ea37dd1f30de6fcef9e018b2e12e25f9ff
admin <admin@example.com>
2025-02-25 11:54:20 +0100

    made bookingDTO, started on  several methods for hadling booking

--- src/main/java/com/java2024/ecoscape/repositories/BookingRepository.java
+++ src/main/java/com/java2024/ecoscape/repositories/BookingRepository.java
@@ -1,8 +1,8 @@
-package com.java2024.ecoscape.repositories;
+package com.java2024.ecoscape.repository;
 
 import com.java2024.ecoscape.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookingRepository extends JpaRepository<Booking, Long> {
+public interface BookingRepository extends JpaRepository<Booking, String> {
 
 }
