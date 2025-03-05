4d50be0d0f2386e18ed9ad5b27ee472efd87c744
Iasmina Alkashash <iasminakash@hotmail.com>
2025-03-05 16:55:18 +0100

    resolve conflict

--- src/main/java/com/java2024/ecoscape/repositories/BookingRepository.java
+++ src/main/java/com/java2024/ecoscape/repositories/BookingRepository.java
@@ -3,6 +3,6 @@ package com.java2024.ecoscape.repositories;
 import com.java2024.ecoscape.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookingRepository extends JpaRepository<Booking, String> {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 
 }
