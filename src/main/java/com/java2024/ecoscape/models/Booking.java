b6bfdff46519088749cd1482ebc80e3877600f2c
Iasmina Alkashash <iasminakash@hotmail.com>
2025-03-05 20:02:01 +0100

    adjusting generaive value annotations, adjusting column name path

--- src/main/java/com/java2024/ecoscape/models/Booking.java
+++ src/main/java/com/java2024/ecoscape/models/Booking.java
@@ -13,7 +13,7 @@ import java.time.LocalDate;
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "booking_Id")
+    @Column(name = "id")
     private Long id;
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY) // each booking belongs to one user, but a user can have many booking
--- src/main/java/com/java2024/ecoscape/models/Listing.java
+++ src/main/java/com/java2024/ecoscape/models/Listing.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class Listing {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 
